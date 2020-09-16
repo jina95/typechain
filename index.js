fe5a88250634e0fa2e97209808e5f859c9806598
jina95 <spzn02@gmail.com>
2020-09-16 17:55:23 +0900

    #2 Setting TypeScript Up

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// type script
+const name = 'Bill', age = 29, gender = 'male';
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age} and ${gender}`);
+};
+// type script rule
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,13 @@
+// type script
+const name = 'Bill',
+age = 29,
+gender = 'male';
+
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age} and ${gender}`);
+}
+
+sayHi(name, age, gender)
+
+export {}
+// type script rule
